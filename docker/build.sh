294034009cc95d4b74f5c1f67f53085b5b3743b3
Piotrek Dubiel <piotr.dubiel@polidea.com>
2017-03-10 16:31:05 +0100

    Just electron-builder

--- docker/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-docker build -t electronuserland/electron-builder:base docker/base
-
-docker build -t electronuserland/electron-builder:7 -t electronuserland/electron-builder:latest docker/7
-docker build -t electronuserland/electron-builder:6 docker/6
-
-docker build -t electronuserland/electron-builder:wine docker/wine
